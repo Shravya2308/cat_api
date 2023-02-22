21c9c37624eeb1309e2954fa93e31f7fa34549c6
Shravya2308 <shravyashetty2308@gmail.com>
2023-02-22 15:27:54 +0530

    okie
    okie

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-alert(1);
+
 let generate_btn = document.querySelector(".generate");
+let meow_sound = document.querySelector(".ding");
 generate_btn.addEventListener("click", fetchPics);
+meow_sound.addEventListener("click", ding);
 
 function fetchPics(){
    fetch("https://api.thecatapi.com/v1/images/search")
@@ -12,8 +14,17 @@ function fetchPics(){
         catImgEl.setAttribute('src',`${catImgUrl}`)
         catImgEl.classList.add("showcase")  
         let catDiv = document.querySelector('.catDiv');
+        catDiv.innerHTML=''
         catDiv.appendChild(catImgEl)
         
     })
     .catch(err => console.log(err))
 }
+
+function ding() {
+    var sound = new  Audio("Cute-cat-meow-sound.mp3");  
+    sound.play();
+}
+
+
+
